055fcae03b2c25a08d3447d45ff87a08bd1b0c79
mariamekpenyong <mariamekpenyong@yahoo.com>
2015-05-22 14:57:05 +0100

    C++assignment

---
+++ c++assignment#1/mimi c++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace  std;
+
+int main()
+{
+	int number2, number3;
+	cout << "enter an interger: ";
+	cin >> number2;
+	
+	number3 = number3 / 2;
+	if (number2 % 2 == 1)
+	cout << number2 << " is an odd number\n";
+	
+	else
+	cout << number2 << " is an even number\n";
+	return 0;
+}
---
+++ c++assignment#1/mimi c++ 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int firstnumber;
+	int secondnumber;
+	int thirdnumber;
+	
+	cout <<"enter a number ";
+	cin >> firstnumber;
+	
+	cout <<"enter a second number: ";
+	cin >> secondnumber;
+	
+	thirdnumber = firstnumber / secondnumber;
+	if (firstnumber % secondnumber == 0)
+	cout << firstnumber <<" is a multiple of " << secondnumber;
+	
+	else
+	cout << firstnumber << "is not a multiple of"<< secondnumber;
+	
+	return 0;
+}
+	
+	
+	
+
